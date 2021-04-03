ca5864d4c9d4cc2a10650b0eb631c84819a62e7a
Arthur Hernandes <61216171+ArthurHSsouza@users.noreply.github.com>
2021-04-03 00:01:41 -0300

    Update dbFactory.js

--- src/db/dbFactory.js
+++ src/db/dbFactory.js
@@ -1,4 +1,5 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
 
 export default class DBFactory{
 
@@ -6,12 +7,13 @@ export default class DBFactory{
     
     constructor(){
 
-        this.connection = mysql.createPool({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'classroom'
-
+        dotenv.config();
+        this.connection = mysql.createPool({ 
+            
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
         });
     }
     
